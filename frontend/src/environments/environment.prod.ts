afcfe23607f29e44655133c4908c8faf04cd4699
Leah Achieng <olaachieng123@gmail.com>
2025-09-13 03:52:54 +0300

    ...

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'YOUR_PROD_GOOGLE_MAPS_API_KEY',
-  apiBaseUrl: 'https://sendit-courier-service-udmw.onrender.com'
+  apiBaseUrl: 'https://sendit-courier-service-udmw.onrender.com/api'
 };
